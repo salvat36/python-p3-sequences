98eebc32c89dc3ae4f8080c1199dcce8252e02a2
Ryan <salvat36@gmail.com>
2023-05-22 11:06:48 -0500

    passing labs

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    sequence = []
+    if length > 0:
+        sequence.append(0)
+    if length > 1:
+        sequence.append(1)
+    if length >= 3:
+        for i in range (length - 2):
+            sequence.append(sequence[-1] + sequence[-2])
+    print(sequence)
+    return sequence
+
+
+
+
+
+    # 0, 1, 1, 2, 3, 5, 8, 13, 21, 34,
